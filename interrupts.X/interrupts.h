6c5e7d0a62eebab069a224b96f5a21d594548c72
serek90 <43785920+serek90@users.noreply.github.com>
2019-11-07 22:16:07 +0100

    add buttons

---
+++ interrupts.X/interrupts.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   interrupts.h
+ * Author: PC
+ *
+ * Created on November 2, 2019, 4:50 PM
+ */
+
+#ifndef INTERRUPTS_H
+#define	INTERRUPTS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* INTERRUPTS_H */
+
---
+++ interrupts.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: PC
+ *
+ * Created on November 1, 2019, 6:59 PM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
